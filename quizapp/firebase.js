4748b2e963f4abc0c172ee83a2df01e9d941edbb
Tulio Salvatierra <salvacorp@gmail.com>
2023-10-29 08:45:52 -0500

    first commit

---
+++ quizapp/firebase.js
@@ -0,0 +1,21 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyAqoARkOFjjubQJeE-_F3DDYmFA262oHkw",
+  authDomain: "reactquiz-a7195.firebaseapp.com",
+  projectId: "reactquiz-a7195",
+  storageBucket: "reactquiz-a7195.appspot.com",
+  messagingSenderId: "902950590761",
+  appId: "1:902950590761:web:7d2e22908688061f9d3f38",
+  measurementId: "G-JF3GPNY67R",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
